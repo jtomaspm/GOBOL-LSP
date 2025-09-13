d9d6b7b10d8e0f12cccb9d23985d7cad8cbd6e0b
João Machado <joaotomachado@proton.me>
2025-09-13 12:54:32 +0100

    change decode

--- pkg/rpc/rpc.go
+++ pkg/rpc/rpc.go
@@ -21,25 +21,12 @@ func EncodeMessage(msg any) string {
 
 // Returns the method name and content length
 func DecodeMessage(data []byte) (string, []byte, error) {
-	header, content, found := bytes.Cut(data, []byte("\r\n\r\n"))
-	if !found {
-		return "", nil, fmt.Errorf("invalid message: missing header-body separator: %s", data)
-	}
-
-	contentLengthBytes := header[len("Content-Length: "):]
-	contentLength, err := strconv.Atoi(string(contentLengthBytes))
-	if err != nil {
-		return "", nil, fmt.Errorf("invalid Content-Length: %v", err)
-	}
-
-	content = content[:contentLength]
-
 	var baseMessage RPCMessage
-	if err = json.Unmarshal(content, &baseMessage); err != nil || baseMessage.Method == "" {
+	if err := json.Unmarshal(data, &baseMessage); err != nil || baseMessage.Method == "" {
 		return "", nil, fmt.Errorf("invalid JSON content: %v", err)
 	}
 
-	return baseMessage.Method, content, nil
+	return baseMessage.Method, data, nil
 }
 
 func Split(data []byte, _ bool) (advance int, token []byte, err error) {
