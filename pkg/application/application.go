a854ccc7e36e5b6a3816f4091d5a1e5f5fbeaff4
João Machado <joaotomachado@proton.me>
2025-09-14 13:18:25 +0100

    refactor

--- pkg/application/application.go
+++ pkg/application/application.go
@@ -54,62 +54,78 @@ func (app *Application) handleMessage(method string, content []byte) {
 	app.logger.Printf("Received message: [%s] %s", method, content)
 	switch method {
 	case "initialize":
-		var request lsp.InitializeRequest
-		if err := json.Unmarshal(content, &request); err != nil {
-			app.logger.Printf("Error: %s", err)
-		}
-		app.logger.Printf("Connected: %s [%s]", request.Params.ClientInfo.Name, request.Params.ClientInfo.Version)
-		response := lsp.NewInitializeResponse(request.ID)
-		app.writeResponse(response)
+		app.handleInitialize(content)
 	case "textDocument/didOpen":
-		var request lsp.DidOpenTextDocumentNotification
-		if err := json.Unmarshal(content, &request); err != nil {
-			app.logger.Printf("Error: %s", err)
-		}
-		app.logger.Printf("Opened: %s", request.Params.TextDocument.URI)
-		app.state.OpenDocument(request.Params.TextDocument.URI, request.Params.TextDocument.Text)
+		app.handleTDDidOpen(content)
 	case "textDocument/didChange":
-		var request lsp.DidChangeTextDocumentNotification
-		if err := json.Unmarshal(content, &request); err != nil {
-			app.logger.Printf("Error: %s", err)
-		}
-		app.logger.Printf("Changed: %s", request.Params.TextDocument.URI)
-		for _, change := range request.Params.ContentChanges {
-			app.state.UpdateDocument(request.Params.TextDocument.URI, change.Text)
-		}
+		app.handleTDDidChange(content)
 	case "textDocument/hover":
-		var request lsp.HoverRequest
-		if err := json.Unmarshal(content, &request); err != nil {
-			app.logger.Printf("Error: %s", err)
-		}
-		app.logger.Printf("Hover: %s", request.Params.TextDocument.URI)
-		response := lsp.HoverResponse{
-			Response: lsp.Response{
-				ID:  &request.ID,
-				RPC: "2.0",
-			},
-			Result: lsp.HoverResult{
-				Contents: "",
-			},
-		}
-		app.state.Hover(request.Params.TextDocument.URI, request.Params.Position, &response)
-		app.writeResponse(response)
+		app.handleTDHover(content)
 	case "textDocument/definition":
-		var request lsp.DefinitionRequest
-		if err := json.Unmarshal(content, &request); err != nil {
-			app.logger.Printf("Error: %s", err)
-		}
-		app.logger.Printf("Definition: %s", request.Params.TextDocument.URI)
-		response := lsp.DefinitionResponse{
-			Response: lsp.Response{
-				ID:  &request.ID,
-				RPC: "2.0",
-			},
-			Result: lsp.Location{},
-		}
-		app.state.Definition(request.Params.TextDocument.URI, request.Params.Position, &response)
-		app.writeResponse(response)
+		app.handleTDDefinition(content)
 	}
+}
+
+func (app *Application) handleInitialize(content []byte) {
+	var request lsp.InitializeRequest
+	if err := json.Unmarshal(content, &request); err != nil {
+		app.logger.Printf("Error: %s", err)
+	}
+	app.logger.Printf("Connected: %s (%s)", request.Params.ClientInfo.Name, request.Params.ClientInfo.Version)
+	response := lsp.NewInitializeResponse(request.ID)
+	app.writeResponse(response)
+}
+
+func (app *Application) handleTDDidOpen(content []byte) {
+	var request lsp.DidOpenTextDocumentNotification
+	if err := json.Unmarshal(content, &request); err != nil {
+		app.logger.Printf("Error: %s", err)
+	}
+	app.state.OpenDocument(request.Params.TextDocument.URI, request.Params.TextDocument.Text)
+}
+
+func (app *Application) handleTDDidChange(content []byte) {
+	var request lsp.DidChangeTextDocumentNotification
+	if err := json.Unmarshal(content, &request); err != nil {
+		app.logger.Printf("Error: %s", err)
+	}
+	for _, change := range request.Params.ContentChanges {
+		app.state.UpdateDocument(request.Params.TextDocument.URI, change.Text)
+	}
+}
+
+func (app *Application) handleTDHover(content []byte) {
+	var request lsp.HoverRequest
+	if err := json.Unmarshal(content, &request); err != nil {
+		app.logger.Printf("Error: %s", err)
+	}
+	response := lsp.HoverResponse{
+		Response: lsp.Response{
+			ID:  &request.ID,
+			RPC: "2.0",
+		},
+		Result: lsp.HoverResult{
+			Contents: "",
+		},
+	}
+	app.state.Hover(request.Params.TextDocument.URI, request.Params.Position, &response)
+	app.writeResponse(response)
+}
+
+func (app *Application) handleTDDefinition(content []byte) {
+	var request lsp.DefinitionRequest
+	if err := json.Unmarshal(content, &request); err != nil {
+		app.logger.Printf("Error: %s", err)
+	}
+	response := lsp.DefinitionResponse{
+		Response: lsp.Response{
+			ID:  &request.ID,
+			RPC: "2.0",
+		},
+		Result: lsp.Location{},
+	}
+	app.state.Definition(request.Params.TextDocument.URI, request.Params.Position, &response)
+	app.writeResponse(response)
 }
 
 func (app *Application) writeResponse(msg any) {
